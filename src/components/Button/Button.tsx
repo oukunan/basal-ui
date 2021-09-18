c38c5aab408844cb694ab1092e46999c03c25fc9
Kunanan <kunanan.tw@gmail.com>
2021-09-18 11:44:28 +0700

    Setup `prettier`

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import "./button.css";
+import React from 'react'
+import './button.css'
 
 export interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
    */
-  primary?: boolean;
+  primary?: boolean
   /**
    * What background color to use
    */
-  backgroundColor?: string;
+  backgroundColor?: string
   /**
    * How large should the button be?
    */
-  size?: "small" | "medium" | "large";
+  size?: 'small' | 'medium' | 'large'
   /**
    * Button contents
    */
-  label: string;
+  label: string
   /**
    * Optional click handler
    */
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 /**
@@ -30,25 +30,25 @@ export interface ButtonProps {
 const Button = ({
   primary = true,
   backgroundColor,
-  size = "medium",
+  size = 'medium',
   onClick,
   label,
 }: ButtonProps) => {
   const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary'
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
       )}
       style={backgroundColor ? { backgroundColor } : {}}
       onClick={onClick}
     >
       {label}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
