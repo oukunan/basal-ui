96782c585d12bab52704d2a5d4d5065c95b27578
Kunanan <kunanan.tw@gmail.com>
2021-09-28 15:26:39 +0700

    Forward ref with breadcrumb

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -13,30 +13,32 @@ type BreadcrumbProps = {
   separator?: React.ReactNode
 }
 
-export default function Breadcrumb(props: BreadcrumbProps) {
-  const { separator = '/', links } = props
+export default React.forwardRef<HTMLElement, BreadcrumbProps>(
+  function Breadcrumb(props, ref) {
+    const { separator = '/', links } = props
 
-  return (
-    <nav aria-label="Breadcrumb">
-      <OrderList>
-        {links.reduce((acc: React.ReactNode[], link, index) => {
-          if (index < links.length - 1) {
-            acc = acc.concat(
-              <BreadcrumbLink key={`item-${index}`} link={link} />,
-              <BreadcrumbSeparator
-                key={`separator-${index}`}
-                separator={separator}
-              />
-            )
-          } else {
-            acc = acc.concat(
-              <BreadcrumbLink key={`item-${index}`} link={link} lastLink />
-            )
-          }
+    return (
+      <nav aria-label="Breadcrumb" ref={ref}>
+        <OrderList>
+          {links.reduce((acc: React.ReactNode[], link, index) => {
+            if (index < links.length - 1) {
+              acc = acc.concat(
+                <BreadcrumbLink key={`item-${index}`} link={link} />,
+                <BreadcrumbSeparator
+                  key={`separator-${index}`}
+                  separator={separator}
+                />
+              )
+            } else {
+              acc = acc.concat(
+                <BreadcrumbLink key={`item-${index}`} link={link} lastLink />
+              )
+            }
 
-          return acc
-        }, [])}
-      </OrderList>
-    </nav>
-  )
-}
+            return acc
+          }, [])}
+        </OrderList>
+      </nav>
+    )
+  }
+)
